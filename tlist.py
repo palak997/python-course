f2e23b1e875751233cd211b21e016ef573909bfb
palak997 <shuklapalak28@gmail.com>
2022-07-18 16:58:23 +0530

    set

--- tlist.py
+++ tlist.py
@@ -2,12 +2,12 @@ from turtle import *
 speed('fastest')
 colors = ['red','blue','pink','yellow','brown' ,'black ','green','voilet','orange']
 
-for i in range (500):
+for i in range (400):
     s = len (colors)
     c = colors[i % s]
     pencolor(c)
     forward(i + 10)
     left(360/s)
     for j in range (s):
-        forward(200)
+        forward(100)
         left(360/s)
